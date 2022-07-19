ca7d06f98fabcafbb2a2e5036d3024c4175ba9c0
Dvd-Znf <david.zanfir.trove@gmail.com>
2022-07-19 14:49:21 +0300

    Adaugat Clasa_XI An 2018

---
+++ Clasa_XI/2018/script.js
@@ -0,0 +1,42 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+} 
+
+function show1() {
+    document.getElementById("iframeContent").src = "../res/ResurseHTML/bastion1.jpg";
+    document.getElementById("btn1").style.color = 'red'
+}
+
+function show2() {
+    document.getElementById("iframeContent").src = "../res/ResurseHTML/bastion2.JPG";
+    document.getElementById("btn2").style.color = 'red'
+}
+
+function show3() {
+    document.getElementById("iframeContent").src = "../res/ResurseHTML/bastion3.jpg";
+    document.getElementById("btn3").style.color = 'red'
+}
+
+function show4() {
+    document.getElementById("iframeContent").src = "../res/ResurseHTML/bastion4.jpg";
+    document.getElementById("btn4").style.color = 'red'
+}
+
+function show5() {
+    document.getElementById("iframeContent").src = "../res/ResurseHTML/bastion5.jpg";
+    document.getElementById("btn5").style.color = 'red'
+}
+
+function show6() {
+    document.getElementById("iframeContent").src = "../res/ResurseHTML/bastion6.jpg";
+    document.getElementById("btn6").style.color = 'red'
+}
+
+function show7() {
+    document.getElementById("iframeContent").src = "../res/ResurseHTML/bastion7.JPG";
+    document.getElementById("btn7").style.color = 'red'
+}
